20f331bcddc80359ff740a29df7f971caee61952
Brian C. Lindner <brian@bclindner.com>
2018-10-30 14:56:18 -0400

    Updates

---
+++ precache-manifest.fc368a6fae57429409d30847724736eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ff129230de1e4e20067daf0c9031301",
+    "url": "/web-validator/static/media/bottombg.8ff12923.png"
+  },
+  {
+    "revision": "04e2d78e01511ffc0c6baeffce325136",
+    "url": "/web-validator/static/media/background2.04e2d78e.png"
+  },
+  {
+    "revision": "5952e72cbd3224a0d1f5",
+    "url": "/web-validator/static/js/runtime~main.5952e72c.js"
+  },
+  {
+    "revision": "0429292c069e3fc38560",
+    "url": "/web-validator/static/js/main.0429292c.chunk.js"
+  },
+  {
+    "revision": "132b10a67294f9289565",
+    "url": "/web-validator/static/js/1.132b10a6.chunk.js"
+  },
+  {
+    "revision": "0429292c069e3fc38560",
+    "url": "/web-validator/static/css/main.14cb578a.chunk.css"
+  },
+  {
+    "revision": "132b10a67294f9289565",
+    "url": "/web-validator/static/css/1.66cef6ff.chunk.css"
+  },
+  {
+    "revision": "bdf2af1dc0d82c0710a3de8bdd02b482",
+    "url": "/web-validator/index.html"
+  }
+];
